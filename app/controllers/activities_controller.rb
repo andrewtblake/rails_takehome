67afeb1b4d711465ff4517d0b7e5e93334359492
Andrew Blake <andrew.blake@rradar.com>
2022-02-28 22:11:10 +0000

    Provide almost working ActivitiesController -- session-based stepping

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -12,39 +12,73 @@ class ActivitiesController < ApplicationController
 
   # GET /activities/new
   def new
-    @activity = Activity.new
+    session[:activity_params] ||= {}
+    @activity = Activity.new(session[:activity_params])
+    @activity.current_step = session[:activity_step]
   end
 
   # GET /activities/1/edit
   def edit
+    session[:activity_params].deep_merge!(activity_params) if params[:activity]
+    @activity.current_step = session[:activity_step]
   end
 
   # POST /activities or /activities.json
   def create
-    @activity = Activity.new(activity_params)
-
-    respond_to do |format|
-      if @activity.save
-        format.html { redirect_to activity_url(@activity), notice: "Activity was successfully created." }
-        format.json { render :show, status: :created, location: @activity }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @activity.errors, status: :unprocessable_entity }
+    session[:activity_params].deep_merge!(activity_params)
+    @activity = Activity.new(session[:activity_params])
+    @activity.current_step = session[:activity_step]
+    case params[:commit]
+    when "Save"
+      if @activity.all_valid?
+        respond_to do |format|
+          if @activity.save
+            session[:activity_params] = nil
+            session[:activity_step] = nil
+            format.html { redirect_to activities_path, notice: "Activity was successfully created." }
+            format.json { render :show, status: :created, location: @activity }
+          else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @activity.errors, status: :unprocessable_entity }
+          end
+        end
+        return
       end
+    when "Next"
+      @activity.next_step if @activity.valid?
+    when "Back"
+      @activity.previous_step if @activity.valid?
     end
+    session[:activity_step] = @activity.current_step
+    redirect_to new_activity_path
   end
 
   # PATCH/PUT /activities/1 or /activities/1.json
   def update
-    respond_to do |format|
-      if @activity.update(activity_params)
-        format.html { redirect_to activity_url(@activity), notice: "Activity was successfully updated." }
-        format.json { render :show, status: :ok, location: @activity }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @activity.errors, status: :unprocessable_entity }
+    session[:activity_params].deep_merge!(activity_params)
+    @activity.current_step = session[:activity_step]
+    case params[:commit]
+    when "Save"
+      respond_to do |format|
+        if @activity.update(session[:activity_params])
+          session[:activity_params] = nil
+          session[:activity_step] = nil
+          format.html { redirect_to activities_path, notice: "Activity was successfully updated." }
+          format.json { render :show, status: :ok, location: @activity }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @activity.errors, status: :unprocessable_entity }
+        end
       end
+      return
+    when "Next"
+      @activity.next_step if @activity.valid?
+    when "Back"
+      @activity.previous_step if @activity.valid?
     end
+    session[:activity_step] = @activity.current_step
+    puts @activity.current_step.inspect
+    redirect_to edit_activity_path
   end
 
   # DELETE /activities/1 or /activities/1.json
