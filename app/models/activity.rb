98ce24b360b57bb94776f58ecb924386c0cdc68f
Andrew Blake <andrew.blake@rradar.com>
2022-02-28 20:43:24 +0000

    Add basic validations for Activity model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,4 +4,8 @@ class Activity < ApplicationRecord
   def steps
     %w[step_1 step_2 step_3]
   end
+
+  validates :name, presence: true, if: ->(o){ o.current_step == 'step_1' }
+  validates :address, presence: true, if: ->(o){ o.current_step == 'step_2' }
+  validates :starts_at, :ends_at, presence: true, if: ->(o){ o.current_step == 'step_3' }
 end
